d53cc274e08fb038b052da7203bd7ff02cd92893
Viacheslav Osadchiy <wavemeup1@gmail.com>
2019-12-06 02:43:03 +0300

    port and context

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(routes);
+app.use('/itmo',routes);
 
 app.use((err, req, res, next) => {
     if (err.status < 500) {
@@ -18,6 +18,6 @@ app.use((err, req, res, next) => {
     let message = 'Internal Server Error', status = 500;
     return res.status(status).send({message});
 })
-app.listen(8080, "0.0.0.0", (request, response) => {
+app.listen(8081, "0.0.0.0", (request, response) => {
     console.log("Webserver started.");
 });
